5ec5a0b11162cc4a8f53632a6107a59a2c1d8102
jongrover <hello@jonathangrover.com>
2013-09-25 12:01:22 -0400

    make beautiful tables

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ FlatironTwitterApi::Application.routes.draw do
 
   resources :speakers
 
+  root :to => 'speakers#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
