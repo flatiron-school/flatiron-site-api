35f99dbf09b736d7d7496228f875423491a47d2e
jongrover <hello@jonathangrover.com>
2013-09-27 09:26:49 -0400

    update twitter oauth keys

--- config/initializers/twitter_oauth.rb
+++ config/initializers/twitter_oauth.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
 Twitter.configure do |config|
-  config.consumer_key = '6RD2ka7b9aLNtxGA3i8qQ'
-  config.consumer_secret = 'y97KGv7nWLmNiSLp3PdhzsYmKiDpTRZVY41omKTik'
+  config.consumer_key = 'MZ6FoWF91zWW1oRHh0jPrA'
+  config.consumer_secret = 'u7zRCVjO0UNrML0FWUUfZzhTQja1tVB7sfZJFGM'
   config.oauth_token = '21532884-eEgDB8VvA1wbKkgDiEpnPD4AJWQ118FsFMzZG9mpc'
   config.oauth_token_secret = 'dsuz4JUXT00ALHdhozhUYRm17pFfpPjKQS5LOTL8'
 end
