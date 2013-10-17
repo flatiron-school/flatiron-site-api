c3031ba6cc609120102a52db74d3508af654babb
jongrover <hello@jonathangrover.com>
2013-10-17 16:54:51 -0400

    remove paperclip mods

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
-Paperclip::Attachment.default_options.merge!(
-  path: ':class/:attachment/:id/:style/:filename'
-)
+# Paperclip::Attachment.default_options.merge!(
+#   path: ':class/:attachment/:id/:style/:filename'
+# )
