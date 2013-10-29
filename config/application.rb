ca9520ee658fd8a86ed5d4830fcd740a0d177edb
jongrover <hello@jonathangrover.com>
2013-10-29 17:42:35 -0400

    add config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+  config.assets.initialize_on_precompile = false
 end
 
 module FlatironTwitterApi
