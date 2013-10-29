55735c0fcad85da824522d0995ebfff26a6a2a0c
jongrover <hello@jonathangrover.com>
2013-10-29 18:25:42 -0400

    update :url path in model and set precompile of assests before deploy in application.rb

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,6 +1,6 @@
 class Employer < ActiveRecord::Base
   attr_accessible :after_flatiron, :before_flatiron, :student, :title, :position, :img
-  has_attached_file :img, :styles => { :medium => "450x309>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :img, :styles => { :medium => "450x309>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :url => ":s3_domain_url"
 
   validates_uniqueness_of :title
   validates_presence_of :position
